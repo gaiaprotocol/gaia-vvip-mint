abeafd477c49355842d3305f4953daf3c1a0795c
Sim Young Jae <hanul@hanul.me>
2022-04-20 16:19:13 +0900

    dev

--- lib/view/Home.d.ts
+++ lib/view/Home.d.ts
@@ -1,6 +1,7 @@
 import { View, ViewParams } from "skydapp-common";
 export default class Home implements View {
     private container;
+    private status;
     private nftList;
     private tokenIds;
     private interval;
--- src/view/Home.ts
+++ src/view/Home.ts
@@ -11,6 +11,7 @@ export default class Home implements View {
 
     private container: DomNode;
 
+    private status: DomNode;
     private nftList: DomNode;
 
     private tokenIds: number[] = [];
@@ -24,6 +25,7 @@ export default class Home implements View {
             el("h2", "for VVIP"),
             el("h3", "sale NFT"),
             el("h4", "price: 1,000 KLAY"),
+            this.status = el("p", "..."),
             this.nftList = el(".nft-container"),
         ));
 
@@ -35,8 +37,15 @@ export default class Home implements View {
 
     private loadNFTsDebouncer: Debouncer = new Debouncer(200, () => this.loadNFTs());
 
-    private loadSales(): void {
-
+    private async loadSales() {
+        const step = (await VVIPMinterContract.step()).toNumber();
+        if (step === 2) {
+            this.status.empty().appendText("status: 퍼블릭 세일 중입니다.");
+        } else if (step === 1) {
+            this.status.empty().appendText("status: 프리세일 중입니다.");
+        } else {
+            this.status.empty().appendText("status: 아직 판매중이 아닙니다.");
+        }
     }
 
     private async loadNFTs(): Promise<void> {
